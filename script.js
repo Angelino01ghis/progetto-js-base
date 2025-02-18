0d3ad2b51a2e63ac36cd526d88b5e8c98ab23655
Angelino01ghis <162619173+Angelino01ghis@users.noreply.github.com>
2025-02-18 18:57:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const counterDisplay = document.getElementById("counter-display");
-const incrementButton = document.getElementById("increment-btn");
-const decrementButton = document.getElementById("decrement-btn");
+// assegniamo al counter 0
+counter = 0
 
-let counterValue = 0;
+// scriviamo la funzione di descrescita
+function left (){
+    const leftButton = document.getElementById("decrement-btn") // assegno una costante
+    leftButton.addEventListener("click", function(){
+        counter --; //assegno ad un evento la funzione di decremento
+        loadCounter() // finito la dichiarazione della funzione aggiorno il counter con funzione
+    })
+}
 
-incrementButton.addEventListener("click", () => {
-    counterValue++;
-    counterDisplay.textContent = counterValue;
-});
+// funzione di incremento
+function right(){
+    const rightButton = document.getElementById("increment-btn") // assegno una costante
+    rightButton.addEventListener("click", function(){
+        counter ++; //assegno ad un evento la funzione di incremento
+        loadCounter() // finito la dichiarazione della funzione aggiorno il counter con funzione
+    })
+}
 
-decrementButton.addEventListener("click", () => {
-    counterValue--;
-    counterDisplay.textContent = counterValue;
-});
+// aggiorno il display del counter
+function loadCounter(){
+    document.getElementById("counter-display").textContent = counter // questa funzione andrà a prendere il valore nel DOM di counter display e modificherà il contenuto assegnadogli il valore di counter
+}
+
+//richiamo le funzioni per eseguirle
+left();
+right();
